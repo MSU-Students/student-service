9fc364a2e9b099c7dd3ff63e2ea6d396574e22cf
Your NameJawharah2000 <jawiees19@gmail.com>
2021-11-09 07:37:05 +0800

    updating homepage

--- src/pages/Forgot_Password.vue
+++ src/pages/Forgot_Password.vue
@@ -1,17 +1,13 @@
 <template>
-<<<<<<< HEAD
-  <q-page class="bg-image">
-=======
   <q-page class="bg= blue">
->>>>>>> 1ef546ee77ad4f7ee4047e4d7c5070438421eeb7
     <q-card class="q-pa-sm absolute-center bg-transparent" style="width: 400px">
       <q-card-section class="q-pa-sm">
         <q-card-section>
-          <q-input 
+          <q-input
           color="blue-grey-10"
-          v-model="label" 
+          v-model="label"
           :type="isPwd ? 'password' : 'pass'"
-          label="New password"> 
+          label="New password">
           <template v-slot:append>
               <q-icon
                 :name="isPwd ? 'visibility_off' : 'visibility'"
@@ -19,56 +15,38 @@
                 @click="isPwd = !isPwd"
               />
             </template>
-            
+
           </q-input>
 
-          <q-input 
+          <q-input
           color="blue-grey-10"
-          v-model="label" 
+          v-model="label"
           :type="isPwd ? 'password' : 'pass'"
           label="Confirm new password"
-          > 
-<<<<<<< HEAD
-          <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-=======
->>>>>>> 1ef546ee77ad4f7ee4047e4d7c5070438421eeb7
-          
+          >
+
           </q-input>
         </q-card-section>
            <div  class="q-pa-x3 q-gutter-md flex flex-center">
             <q-card-actions vertical align="center">
             <q-btn
-            class="glossy" 
-<<<<<<< HEAD
-             color="grey-14 text-black" 
-=======
-            color="cyan-5" 
->>>>>>> 1ef546ee77ad4f7ee4047e4d7c5070438421eeb7
+            class="glossy"
+            color="grey-14"
            label="Confirm"
            to="SigningPage"
            style="width: 100px"
           >
           </q-btn>
           <q-btn
-            class="glossy" 
-<<<<<<< HEAD
-            color="grey-14 text-black" 
-=======
-            color="cyan-5" 
->>>>>>> 1ef546ee77ad4f7ee4047e4d7c5070438421eeb7
+            class="glossy"
+            color="cyan-5"
             label="Cancel"
             to="SigningPage"
             style="width: 100px"
           >
           </q-btn>
-        </q-card-actions> 
-        </div> 
+        </q-card-actions>
+        </div>
       </q-card-section>
     </q-card>
   </q-page>
--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,70 +1,14 @@
 <template>
-<q-page class="bg-image">
-    <div class="q-pa-sm">
-    <q-carousel
-      swipeable
-      animated
-      v-model="slide"
-      v-model:fullscreen="fullscreen"
-      :autoplay="autoplay"
-      ref="carousel"
+  <div class="q-pa-xl q-gutter-xl">
+    <div class="row justify-between">
 
-      infinite
-       height="600px"
+    <q-parallax
+
+      src="~/assets/23.jpg"
     >
-      <q-carousel-slide :name="1" img-src="~/assets/23.jpg" />
-      <q-carousel-slide :name="2" img-src="~/assets/ORG.png" />
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <h1 class="text-white">CICS</h1>
+    </q-parallax>
 
-      <template v-slot:control>
-        <q-carousel-control
-          position="top-right"
-          :offset="[18, 18]"
-          class="text-white rounded-borders"
-          style="background: rgba(0, 0, 0, .3); padding: 4px 8px;"
-        >
-          <q-toggle dense dark color="orange" v-model="autoplay" label="Auto Play" />
-        </q-carousel-control>
-
-        <q-carousel-control
-          position="bottom-right"
-          :offset="[18, 18]"
-          class="q-gutter-xs"
-        >
-          <q-btn
-            push round dense color="orange" text-color="black" icon="arrow_left"
-            @click="$refs.carousel.previous()"
-          />
-          <q-btn
-            push round dense color="orange" text-color="black" icon="arrow_right"
-            @click="$refs.carousel.next()"
-          />
-          <q-btn
-            push round dense color="white" text-color="primary"
-            :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
-            @click="fullscreen = !fullscreen"
-          />
-        </q-carousel-control>
-      </template>
-    </q-carousel>
+    </div>
   </div>
-
-</q-page>
-  
 </template>
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup () {
-    return {
-      slide: ref(1),
-      autoplay: ref(false),
-       fullscreen: ref(false)
-      
-      
-    }
-  }
-}
-</script>
